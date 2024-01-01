5617478375dcaa9da7d431dc22cebe5acc18c9e2
Chad Provencher <thechad802@gmail.com>
2024-01-01 14:30:35 -0500

    added setup.py configs

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="kryslib",
@@ -6,17 +6,6 @@ setup(
     description="A library for the Krys project",
     author="Chad Provencher",
     author_email="kryses@kryses.com",
-    packages=find_packages(),
-    install_requires=[
-        "asttokens==2.4.1",
-        "colorama==0.4.6",
-        "executing==2.0.1",
-        "icecream==2.1.3",
-        "iniconfig==2.0.0",
-        "packaging==23.2",
-        "pluggy==1.3.0",
-        "Pygments==2.17.2",
-        "pytest==7.4.3",
-        "six==1.16.0",
-    ],
+    packages=["kryslib"],
+    package_dir={"kryslib": "src/kryslib"},
 )
